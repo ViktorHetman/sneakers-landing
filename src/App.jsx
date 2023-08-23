fae4e1334a07a6dddd546ab66c0f4eaf8829eb6c
Viktor <vitya.getman.79@gmail.com>
2023-08-23 12:49:50 +0200

    entire structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import {
+  CustomerReviews,
+  Hero,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
 const App = () => {
   return (
     <main className="relative">
-      <nav>Nav</nav>
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">Popular Products</section>
-      <section className="padding">Super Quality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">Special Offer</section>
-      <section className="bg-pale-blue padding">Customer Reviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <footer className="bg-black padding-x  padding-t pb-8">Footer</footer>
+      <nav>{/* <Nav /> */}</nav>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <footer className="bg-black padding-x  padding-t pb-8">
+        <Footer />
+      </footer>
     </main>
   );
 };
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+export {
+  CustomerReviews,
+  Hero,
+  SpecialOffer,
+  PopularProducts,
+  Services,
+  Footer,
+  SuperQuality,
+  Subscribe,
+};
