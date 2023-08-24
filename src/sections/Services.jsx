2d082d03146e0fc82d9ebc8165f9b4b602257e86
Viktor <vitya.getman.79@gmail.com>
2023-08-24 12:36:04 +0200

    services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,15 @@
+import ServiceCard from "../components/ServiceCard";
+
+import { services } from "../constants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justfiy-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
